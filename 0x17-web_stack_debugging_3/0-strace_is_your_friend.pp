9950a0ccdcfad24f286b978d630156d55f52411d
esraa200113 <esraa200113@gmail.com>
2024-09-26 01:34:10 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,14 @@
-# This Puppet manifest fixes the Apache 500 error caused by missing PHP modules and incorrect directory permissions.
-
-# Ensure that the php-mysql package (required for WordPress) is installed
-package { 'php-mysql':
+# Ensure Apache is installed and running
+package { 'apache2':
   ensure => installed,
 }
 
-# Set correct permissions for the WordPress directory
+# Ensure PHP and necessary PHP modules for WordPress are installed
+package { ['php', 'php-mysql', 'php-curl', 'libapache2-mod-php']:
+  ensure => installed,
+}
+
+# Ensure WordPress directory is correctly set up with proper permissions
 file { '/var/www/html':
   ensure  => directory,
   owner   => 'www-data',
@@ -14,10 +17,18 @@ file { '/var/www/html':
   recurse => true,
 }
 
-# Restart Apache service to apply changes
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  subscribe  => [ Package['php-mysql'], File['/var/www/html'] ],
+# Ensure Apache is pointing to the correct DocumentRoot for the WordPress site
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => file,
+  content => template('apache/default.conf.erb'),
+  notify  => Service['apache2'],  # Reload Apache if config changes
 }
+
+# Restart Apache to apply changes
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => [ Package['php'], File['/var/www/html'], File['/etc/apache2/sites-available/000-default.conf'] ],
+}
+
 
